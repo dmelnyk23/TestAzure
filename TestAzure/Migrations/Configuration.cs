15d221dcd2c151629c4dd66e326d44aec99c8f8b
Dima Melnyk <dmelnyk231098@gmail.com>
2018-11-20 16:35:33 +0200

    Add project files.

---
+++ TestAzure/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TestAzure.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TestAzure.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TestAzure.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
